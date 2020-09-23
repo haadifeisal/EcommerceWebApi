92da8266dd842bc069b8fdcf2ccc5ad844761621
Heybe Feisal <46941322+heybefeisal@users.noreply.github.com>
2020-09-23 13:40:26 +0200

    Added Test project.
    Lost my previous Test Project, created a new one.

---
+++ EcommerceWebApi.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EcommerceWebApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var math = 1 + 1;
+            Assert.IsTrue(math == 2);
+        }
+    }
+}
